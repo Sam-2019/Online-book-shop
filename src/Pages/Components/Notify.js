fc3f01783ca3a518c26c538ca037381424e3770b
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-01-30 09:18:52 +0000

    code update

--- src/Pages/Components/Notify.js
+++ src/Pages/Components/Notify.js
@@ -7,9 +7,9 @@ const Notify = ({ message, close }) => {
   return (
     <div className="notify-wrapper ">
       <div className="notify ">
-        <div className="notify-close ">
+        {/* <div className="notify-close ">
           <Close width={25} height={25} action={close} />
-        </div>
+        </div> */}
 
         <div className="notify-main">{message}</div>
       </div>
