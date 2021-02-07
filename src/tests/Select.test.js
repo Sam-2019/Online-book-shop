ca2c9616e68149ba2394b736068b0e5c074d2724
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-07 19:08:26 +0000

    updated Select component

--- src/tests/Select.test.js
+++ src/tests/Select.test.js
@@ -1,10 +1,46 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+jest.mock("axios");
 
 import Select from "../Pages/Components/Select";
 
 describe("Select", () => {
-  test("renders App component", () => {
+  test("fetches data from an API and displays them", async () => {
+    // const data = [
+    //   {
+    //     id: "0",
+    //     unique_id: "0",
+    //     location: "Pick your location",
+    //     fee: "0.00",
+    //     disabled: true,
+    //   },
+    //   {
+    //     id: "1",
+    //     unique_id: "5f97102cd9ba86.00000001",
+    //     location: "Community 1",
+    //     fee: "7.00",
+    //     disabled: false,
+    //   },
+    //   {
+    //     id: "2",
+    //     unique_id: "5f97102cd9ba86.00000002",
+    //     location: "Community 2",
+    //     fee: "7.00",
+    //     disabled: false,
+    //   },
+    // ];
+
     render(<Select />);
+
+    await screen.findByText(/Pick your location/);
+
+    expect(screen.queryByText(/Pick your location/)).toBeNull();
+
+    fireEvent.change(screen.getByRole("select"), {
+      target: { value: "Pick your location" },
+    });
+    
+    expect(screen.getByText(/Pick your location/)).toBeInTheDocument();
   });
 });
