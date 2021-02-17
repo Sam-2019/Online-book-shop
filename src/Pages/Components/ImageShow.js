9739827ecd787383e8f71139430d0ea21cbad1c9
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-17 18:46:08 +0000

    updated Profile Image  component

--- src/Pages/Components/ImageShow.js
+++ src/Pages/Components/ImageShow.js
@@ -35,20 +35,18 @@ const ImageShow = () => {
   };
 
   const Change = (e) => {
+    setLoading(true);
     e.preventDefault();
 
     let reader = new FileReader();
-
-    reader.onloadstart = () => setLoading(true);
-
     let pic = e.target.files[0];
 
-    reader.onload = (e) => {
-      setFile(e.target.files[0]);
-    };
+    // reader.onload = (e) => {
+    //   setFile(e.target.result);
+    // };
 
     reader.onloadend = () => {
-      // setFile(e.target.files[0]);
+      setFile(e.target.files[0]);
       setimagePreviewUrl(reader.result);
       setLoading(false);
     };
@@ -60,34 +58,40 @@ const ImageShow = () => {
   return (
     <>
       <div className="containerWar">
-        {imagePreviewUrl ? (
-          <>
-            <img src={imagePreviewUrl} alt="Avatar" className=" just-image" />
-          </>
-        ) : (
-          <>
-            <img
-              src="https://images.unsplash.com/photo-1534429068323-d715c5a6d96b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80"
-              alt="Avatar"
-              className=" just-image"
+        <form onSubmit={imageUpload} className="">
+          {imagePreviewUrl ? (
+            <>
+              <img src={imagePreviewUrl} alt="Avatar" className=" just-image" />
+            </>
+          ) : (
+            <>
+              <img
+                src="https://images.unsplash.com/photo-1534429068323-d715c5a6d96b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80"
+                alt="Avatar"
+                className=" just-image"
+              />
+            </>
+          )}
+
+          <div className="middle" onClick={openBox}>
+            <input
+              className="fileInput"
+              type="file"
+              onChange={Change}
+              id="profileImage"
+              accept="image/*"
+              capture="camera"
             />
-          </>
-        )}
 
-        <div className="middle" onClick={openBox}>
-          <input
-            className="fileInput"
-            type="file"
-            onChange={Change}
-            id="profileImage"
-            accept="image/*"
-            capture="camera"
-          />
+            <label htmlFor="profileImage" className="text">
+              {loading ? "Loading.." : "   Change Image"}
+            </label>
+          </div>
 
-          <label htmlFor="profileImage" className="text">
-            {loading ? "Loading.." : "   Change Image"}
-          </label>
-        </div>
+          <button className="uploadImage " type="submit">
+            Upload Image
+          </button>
+        </form>
       </div>
 
       {/* {change ? (
