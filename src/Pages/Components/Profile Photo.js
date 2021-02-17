c21bc39b272e8a5310eb6ff23a3f16674de14771
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-02-17 21:24:54 +0000

    pofile photo update

--- src/Pages/Components/Profile Photo.js
+++ src/Pages/Components/Profile Photo.js
@@ -1,15 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { profliePhoto } from "../endpoints";
 import "./profilePhoto.css";
 
 const ProfilePhoto = ({ class_name }) => {
-  return (
-    <img
-      src="https://images.unsplash.com/photo-1534429068323-d715c5a6d96b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80"
-      alt="Avatar"
-      className={class_name}
-    />
-  );
+  return <img src={profliePhoto} alt="Avatar" className={class_name} />;
 };
 
 export default ProfilePhoto;
