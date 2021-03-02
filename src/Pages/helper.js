4635590b1aae9f3e3431e0c02d4ca871b218971b
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-02 15:45:32 +0000

    updated helper.js

--- src/Pages/helper.js
+++ src/Pages/helper.js
@@ -1,10 +1,10 @@
 import React from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
-import { itemsGet, itemGet, buyerID } from "./endpoints";
+import { itemsGet } from "./endpoints";
 
 const intervalMs = 10000;
-  var formData = new FormData();
+var formData = new FormData();
 
 export async function fetchProjects(page = 0) {
   const { data } = await axios({
@@ -25,23 +25,6 @@ export async function backendData(url, formData) {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return data.data;
-}
-
-export async function productFetch(url, formData) {
-
-  //formData.set("product_unique_id", "574ed359ce2e18.82356121");
-
-  try {
-    const response = await axios({
-      method: "post",
-      url: url,
-      data: formData,
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
 }
 
 export function useProducts() {
