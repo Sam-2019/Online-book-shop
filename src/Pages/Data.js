41bdb68fb635b5fbf149da010fe61757227c9850
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-10 17:42:09 +0000

    added Data for global fetch

---
+++ src/Pages/Data.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import axios from "axios";
+import {
+  dev_site,
+  itemsGet,
+  itemGet,
+  tagsGet,
+  tagGet,
+  userLogin,
+  userRegister,
+  userValidate,
+  userPasswordUpdate,
+  userProfileUpdate,
+  userEmailUpdate,
+  userAccountReset,
+  userAccountVerify,
+  userCreateEmailVerify,
+  userReadEmailVerify,
+  passwordReset,
+  itemSearch,
+  cartAdd,
+  cartGet,
+  cartCount,
+  cartUpdate,
+  cartDelete,
+  cartCheckout,
+  cartSummary,
+  orderCreate,
+  orderHistory,
+  orderDetail,
+  wishCreate,
+  wishList,
+  wishDelete,
+  userWelcome,
+  buyerID,
+} from "./endpoints";
+import { axiosMethod, useLocalStorage } from "./helper";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+  useQueryClient,
+} from "react-query";
+
+//Try destructring the responses i.e. {data}
+
+const instance = axios.create({
+  baseURL: dev_site,
+});
+
+const Data = () => {
+  var formData = new FormData();
+  formData.set("buyer_unique_id", buyerID);
+
+  const [amount, setAmount] = useState(0);
+  const [quantity, setQuantity] = useState(0);
+
+  const queryClient = useQueryClient();
+
+  async function summaryCart(formData) {
+    const { data } = await axiosMethod("post", cartSummary, formData);
+
+    if (data) {
+    } else {
+      return;
+    }
+  }
+
+  useQuery("summaryData", () =>
+    axios({
+      method: "POST",
+      url: cartSummary,
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then((data) => {
+        if (data.data.message === "cart is empty") {
+        } else {
+          setQuantity(Number(data.data.data.total_quantity));
+          setAmount(Number(data.data.data.total_amount));
+          // console.log("Success:", data.data.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+  );
+
+  return {
+    summaryCart,
+    amount,
+    quantity,
+  };
+};
+
+export default Data;
