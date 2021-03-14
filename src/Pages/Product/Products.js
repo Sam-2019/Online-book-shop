14335122ceedb27b890992172f57386a061a7b59
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-14 18:45:18 +0000

    renamed page state, added page views, removed comment code

--- src/Pages/Product/Products.js
+++ src/Pages/Product/Products.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery, QueryClient } from "react-query";
+import styled from "styled-components";
 import { fetchProjects } from "../helper";
 import ProductsItem from "./productsItem";
 import Placeholder from "./Placeholder";
@@ -7,27 +8,44 @@ import "./products.css";
 
 const queryClient = new QueryClient();
 
+const Loading = styled.div`
+display: flex,
+justify-content: center,
+
+
+`;
+
 const Products = () => {
   // Queries
   // const { isLoading, isError, data, error, status } = useProducts();
-  const [page, setPage] = React.useState(0);
+  const [items, setItems] = React.useState(0);
+  const [page, setPages] = React.useState(1);
+
+  var formData = new FormData();
+
+  formData.set("offset", items);
+
   const { status, data, error, isFetching, isPreviousData } = useQuery(
-    ["projects", page],
-    () => fetchProjects(page),
+    ["projects", items],
+    () => fetchProjects(formData),
     {
       keepPreviousData: true,
       staleTime: 5000,
+      cacheTime: 20000,
     }
   );
 
-  // Prefetch the next page!
+
+
+  // Prefetch the next items!
   React.useEffect(() => {
     if (data?.hasMore) {
-      queryClient.prefetchQuery(["projects", page + 1], () =>
-        fetchProjects(page + 1)
+      queryClient.prefetchQuery(["projects", items + 1], () =>
+        fetchProjects(items + 1)
       );
     }
-  }, [data, page]);
+  }, [data, items]);
+
 
   return (
     <>
@@ -37,56 +55,43 @@ const Products = () => {
         ) : status === "error" ? (
           <div>Error: {error.message}</div>
         ) : (
-          // `data` will either resolve to the latest page's data
-          // or if fetching a new page, the last successful page's data
+          // `data` will either resolve to the latest items's data
+          // or if fetching a new items, the last successful items's data
           <div className="products">
             {data.map((items, index) => (
               <ProductsItem key={index} {...items} />
             ))}
           </div>
         )}
-
-        {/* {status === "loading" ? (
-        <>Loading...</>
-      ) : status === "error" ? (
-        <>Error: {error.message}</>
-      ) : (
-        <>
-          {data.map((items, index) => (
-            <ProductsItem key={index} {...items} />
-          ))}
-        </>
-      )} */}
-        {/* {Array(10)
-        .fill()
-        .map((item, index) => (
-          <ProductsItem key={index} index={index} />
-        ))} */}
       </div>
 
-      <div>Current Page: {page + 1}</div>
+      <div> {isFetching ? null : <span> Current Page: {page}</span>}</div>
 
-      <button
-        onClick={() => setPage((old) => Math.max(old - 1, 0))}
-        disabled={page === 0}
-      >
-        Previous Page
-      </button>
+      <div>
+        {isFetching ? null : (
+          <div>
+            <button
+              onClick={() => {
+                setItems((c) => c - 8);
+                setPages((c) => c - 1);
+              }}
+              disabled={items === 0}
+            >
+              Previous Page
+            </button>
+            <button
+              onClick={() => {
+                setItems((c) => c + 8);
+                setPages((c) => c + 1);
+              }}
+            >
+              Next Page
+            </button>
+          </div>
+        )}
+      </div>
 
-      <button
-        onClick={() => {
-          setPage((old) => (data?.hasMore ? old + 1 : old));
-        }}
-        disabled={isPreviousData || !data?.hasMore}
-      >
-        Next Page
-      </button>
-      {
-        // Since the last page's data potentially sticks around between page requests,
-        // we can use `isFetching` to show a background loading
-        // indicator since our `status === 'loading'` state won't be triggered
-        isFetching ? <span> Loading...</span> : null
-      }
+      <div> {isFetching ? <span> Loading...</span> : null}</div>
     </>
   );
 };
