7169e56fe5e2490e5bf13f20c0b33a9ba0c4cf89
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-18 21:17:14 +0000

    created PaymentProcess component

---
+++ src/Pages/Order/PaymentProcess.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Button from "../Components/Button";
+
+const PaymentProcess = ([buttonAction]) => {
+  return (
+    <>
+      <ol className="">
+        <li>Dial *170# on your phone</li>
+        <li>Select MoMoPay &amp; PayBill</li>
+        <li>Select MoMoPay</li>
+        <li>
+          Enter <strong>283051 </strong> as the Merchant ID
+        </li>
+        <li>Enter Reference</li>
+        <li>Enter Your Pin to confirm payment</li>
+      </ol>
+
+      <div className="other-info">
+        Upon successful payment, please use the details of the payment to fill
+        the fields below
+      </div>
+
+      {/* <Button class_name="primary" name="Okay" /> */}
+      <div className="popup-action">
+        <Button class_name="primary" name="Close" action={buttonAction} />
+      </div>
+    </>
+  );
+};
+
+export default PaymentProcess;
