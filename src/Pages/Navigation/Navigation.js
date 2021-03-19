c97c809466874e4f365cb80e82a698ca93958c10
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-19 23:12:01 +0000

    removed search code

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Transition, animated } from "react-spring/renderprops";
+import { useQuery, useQueryClient } from "react-query";
 import Cart from "../Components/Cart";
 import User from "../Components/User";
 import Search from "../Components/Search";
@@ -20,122 +20,95 @@ const Navigation = ({ toggle }) => {
   const { width } = MediaQuery();
 
   const breakpoint = 280;
+  const queryClient = useQueryClient();
+
+  function navSearch() {
+    if (query === "") {
+    } else {
+      history.push(`/search?q=${query}`);
+      queryClient.invalidateQueries("searchContent");
+    }
+  }
+
+  function newSearch() {
+    history.push("/search");
+  }
 
   return (
     <header className="nav-header ">
-      {state ? (
-        <div className="category2 ">
-          <Input
-            type="search"
-            class_name="header-input  "
-            placeholder="Search"
-            value={query}
-            action={(e) => setQuery(e.target.value)}
+      <>
+        <div className="category ">
+          <Justify
+            width={breakpoint < width ? 30 : 20}
+            height={breakpoint < width ? 30 : 20}
+            action={toggle}
           />
-
-          <Button
-            name="Search"
-            class_name="header-primary2"
-            action={() => {
-              if (query === "") {
-              } else {
-                history.push(`/search?q=${query}`);
-              }
-            }}
-          />
-
-          <div className="object-4 cart ">
-            <Close
-              width={breakpoint < width ? 30 : 20}
-              height={breakpoint < width ? 30 : 20}
-              action={() => {
-                setState(false);
-              }}
-            />
-          </div>
+          <div className=" okukus ">OKUKUS</div>
         </div>
-      ) : (
-        <>
-          <div className="category ">
-            <Justify
-              width={breakpoint < width ? 30 : 20}
-              height={breakpoint < width ? 30 : 20}
-              action={toggle}
-            />
-            <div className=" okukus ">OKUKUS</div>
-          </div>
 
-          {width > 540 ? (
+        {width > 540 ? (
+          <>
+            <div className="category ">
+              <Input
+                type="search"
+                class_name="header-input  "
+                placeholder="Search"
+                value={query}
+                action={(e) => setQuery(e.target.value)}
+              />
+
+              <Button
+                name="Search"
+                class_name="header-primary2"
+                action={navSearch}
+              />
+            </div>
+          </>
+        ) : null}
+
+        <div className="category item">
+          {width < 540 ? (
             <>
-              <div className="category ">
-                <Input
-                  type="search"
-                  class_name="header-input  "
-                  placeholder="Search"
-                  value={query}
-                  action={(e) => setQuery(e.target.value)}
-                />
-
-                <Button
-                  name="Search"
-                  class_name="header-primary2"
-                  action={() => {
-                    if (query === "") {
-                    } else {
-                      history.push(`/search?q=${query}`);
-                    }
-                  }}
+              <div className="object-3 search  ">
+                <Search
+                  width={breakpoint < width ? 30 : 20}
+                  height={breakpoint < width ? 30 : 20}
+                  action={newSearch}
                 />
               </div>
             </>
           ) : null}
 
-          <div className="category">
-            {width < 540 ? (
-              <>
-                <div className="object-3 search  ">
-                  <Search
-                    width={breakpoint < width ? 30 : 20}
-                    height={breakpoint < width ? 30 : 20}
-                    action={() => {
-                      setState(true);
-                    }}
-                  />
-                </div>
-              </>
-            ) : null}
-
-            <div className="object-4 cart  ">
-              <Cart
-                width={breakpoint < width ? 30 : 20}
-                height={breakpoint < width ? 30 : 20}
-                action={() => {
-                  history.push("/cart");
-                }}
-              />
-            </div>
-
-            <div className="object-4 user  ">
-              <User
-                width={breakpoint < width ? 30 : 20}
-                height={breakpoint < width ? 30 : 20}
-                action={() => {
-                  history.push("/user/profile");
-                }}
-              />
-            </div>
-
-            <div
-              className="object-4 user  "
-              onClick={() => {
-                history.push("/login");
+          <div className="object-4 cart  ">
+            <Cart
+              width={breakpoint < width ? 30 : 20}
+              height={breakpoint < width ? 30 : 20}
+              action={() => {
+                history.push("/cart");
               }}
-            >
-              Login
-            </div>
+            />
           </div>
-        </>
-      )}
+
+          <div className="object-4 user  ">
+            <User
+              width={breakpoint < width ? 30 : 20}
+              height={breakpoint < width ? 30 : 20}
+              action={() => {
+                history.push("/user/profile");
+              }}
+            />
+          </div>
+
+          <div
+            className="object-4 user  "
+            onClick={() => {
+              history.push("/login");
+            }}
+          >
+            Login
+          </div>
+        </div>
+      </>
     </header>
   );
 };
