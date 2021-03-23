86108a861bca092563fb03a70931475ec4cebd7e
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-23 13:55:27 +0000

    removed unused imports

--- src/Pages/Search/Empty.js
+++ src/Pages/Search/Empty.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import GroupComponent from '../Components/GroupComponent'
+import GroupComponent from "../Components/GroupComponent";
 
-const Empty = ({ message }) => {
+const Empty = () => {
   const desktopQuery = new URLSearchParams(useLocation().search).get("q");
 
   return (
     <div className="search-page">
       <GroupComponent />
-      {/* <div className="text-1">   Sorry we couldn't find any matches for lkmhl;mrl;hm4;lml;hm4;l5hm  </div>
-    <p className="text-2">Please try searching with another term</p> */}
 
       <p className="text-3">No item was found for "{desktopQuery}"</p>
     </div>
