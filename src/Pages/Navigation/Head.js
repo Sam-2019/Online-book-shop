2bec23a8c397d532d97295aa9adfccb930e6dd98
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-23 20:02:36 +0000

    added apis to Drawer

--- src/Pages/Navigation/Head.js
+++ src/Pages/Navigation/Head.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Drawer from "react-motion-drawer";
 import Tag from "../Tags/Tags";
-
+import { slide as Menu } from "react-burger-menu";
 import Navigation from "./Navigation";
 import "./navigation.css";
 
@@ -13,13 +13,35 @@ function Nav() {
   }
   return (
     <>
-      <Drawer open={show} width={300} onChange={Toggle}
-       className="hamburger">
+      <Drawer
+        open={show}
+        width={300}
+        noTouchOpen={true}
+        noTouchClose={true}
+        handleWidth={10}
+        peakingWidth={100}
+        panTolerance={20}
+        offset={0}
+      
+        className="hamburger"
+        overlayClassName="blue"
+        overlayColor="rgba(52, 52, 52, 0.866)"
+      >
         {(val) => <Tag toggle={Toggle} />}
       </Drawer>
+
+      {/* <Show pageWrapId={"page-wrap"} outerContainerId={"App"} /> */}
 
       <Navigation toggle={Toggle} />
     </>
   );
 }
 export default Nav;
+
+const Show = ({ props }) => {
+  return (
+    <Menu {...props}>
+      <Tag />
+    </Menu>
+  );
+};
