202008721ff6d47dbebb1b7e788a4b5370d4548e
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-23 13:29:07 +0000

    added new Searchbox

---
+++ src/Pages/Search/Searchbox2.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useQuery, useQueryClient } from "react-query";
+import { useHistory } from "react-router-dom";
+import { Input } from "../Components/Input";
+import Button from "../Components/Button";
+
+const Searchbox = () => {
+  const [query, setQuery] = React.useState("");
+
+  let history = useHistory();
+
+  function Query() {
+    if (query === "") {
+      // alert();
+    } else {
+      history.push(`/search?q=${query}`);
+    }
+  }
+
+  return (
+    <>
+      <Input
+        type="search"
+        class_name="header-input  "
+        placeholder="Search"
+        value={query}
+        action={(e) => setQuery(e.target.value)}
+      />
+
+      <Button name="Search" class_name="header-primary2" action={Query} />
+    </>
+  );
+};
+
+export default Searchbox;
