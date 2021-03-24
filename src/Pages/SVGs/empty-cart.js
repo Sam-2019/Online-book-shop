f463f0613e4a48e856c1c72ba4fef15de7d0b4f4
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-24 23:14:05 +0000

    renamed component

---
+++ src/Pages/SVGs/empty-cart.js
@@ -0,0 +1,77 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const EmptyCart = (props) => (
+  <div className="svg-item">
+    <svg {...props} width={244} height={244} viewBox="0 0 244 244">
+      <defs>
+        <style>
+          {
+            ".a{fill:#e3e3e3;}.b{fill:#c5c5c5;}.c{fill:#a0b2a2;}.d{fill:#7f8a89;}.e{isolation:isolate;}.f{fill:#a0b3a3;}"
+          }
+        </style>
+      </defs>
+      <circle className="a" cx={122} cy={122} r={122} />
+      <path
+        className="b"
+        d="M195.371,312.926A122.046,122.046,0,0,0,298.955,203.7l-49.164-49.164L191.51,198.328,129.7,136.519l-9.44-2.342L106.021,139.3l52.866,52.866-26.69-.411,2.1,53.521,11.838,11.419-2.22,4.961Z"
+        transform="translate(-55.495 -70.233)"
+      />
+      <path
+        className="c"
+        d="M239.24,246.18H164.9V176.893H280.613l-18.193,54.36C259.463,240.089,250,246.18,239.24,246.18Z"
+        transform="translate(-86.317 -92.592)"
+      />
+      <path
+        className="c"
+        d="M261.46,176.892v69.287h28.321c10.763,0,20.223-6.092,23.18-14.927l18.193-54.36Z"
+        transform="translate(-136.858 -92.592)"
+      />
+      <path
+        className="d"
+        d="M223.651,247.56h-73.47a22.564,22.564,0,0,1-22.539-22.539V148.5a13.518,13.518,0,0,0-13.5-13.5H106.02v-9.037h8.119a22.566,22.566,0,0,1,22.54,22.54v76.522a13.517,13.517,0,0,0,13.5,13.5h73.47v9.037Z"
+        transform="translate(-55.495 -65.932)"
+      />
+      <rect
+        className="d"
+        width={43.558}
+        height={9.037}
+        transform="translate(124.604 172.591)"
+      />
+      <g className="e" transform="translate(85.218 172.591)">
+        <circle
+          className="d"
+          cx={6.536}
+          cy={6.536}
+          r={6.536}
+          transform="translate(4.515 4.514)"
+        />
+        <path
+          className="f"
+          d="M189.871,384.266a11.053,11.053,0,1,1,11.054-11.053A11.065,11.065,0,0,1,189.871,384.266Zm0-13.07a2.016,2.016,0,1,0,2.017,2.017A2.019,2.019,0,0,0,189.871,371.2Z"
+          transform="translate(-178.818 -362.159)"
+        />
+        <circle
+          className="d"
+          cx={6.536}
+          cy={6.536}
+          r={6.536}
+          transform="translate(56.799 4.514)"
+        />
+        <path
+          className="c"
+          d="M299.572,384.266a11.053,11.053,0,1,1,11.053-11.053A11.066,11.066,0,0,1,299.572,384.266Zm0-13.07a2.016,2.016,0,1,0,2.016,2.017A2.019,2.019,0,0,0,299.572,371.2Z"
+          transform="translate(-236.239 -362.159)"
+        />
+      </g>
+    </svg>
+  </div>
+);
+
+export default EmptyCart;
