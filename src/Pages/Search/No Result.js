ea7e176535fe3012399f374379d1283941fdff74
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-24 23:16:33 +0000

    added NoResult component

---
+++ src/Pages/Search/No Result.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import SVGContainer from "../SVGs/SVGcontainer";
+import SearchSVG from "../SVGs/search";
+
+const NoResult = () => {
+  const desktopQuery = new URLSearchParams(useLocation().search).get("q");
+
+  return (
+    <SVGContainer>
+      <SearchSVG />
+      <p className="text-3">
+        Sorry we couldn't find any matches for <b>{desktopQuery}</b>
+      </p>
+    </SVGContainer>
+  );
+};
+
+export default NoResult;
