e40c54ba7fa8f133ea261894a6f4f1f781e1a60d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-24 23:11:57 +0000

    added SVGcontainer.js

---
+++ src/Pages/SVGs/SVGcontainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./svgcontainer.css";
+
+const SVGcontainer = ({ children }) => {
+  return <div className="svg-page">{children}</div>;
+};
+
+export default SVGcontainer;
