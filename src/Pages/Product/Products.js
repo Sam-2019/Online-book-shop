2d3a7f93f4ff114804acb049c90d5c2a68b95946
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-24 20:16:15 +0000

    added styled component & fn for negative page error

--- src/Pages/Product/Products.js
+++ src/Pages/Product/Products.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery, QueryClient } from "react-query";
+
 import styled from "styled-components";
 
 import { fetchProjects } from "../helper";
@@ -7,14 +8,68 @@ import ProductsItem from "./productsItem";
 import Placeholder from "../Placeholders/Products";
 import { itemsGet } from "../endpoints";
 import "./products.css";
+import "./pagination.css";
 
 const queryClient = new QueryClient();
 
 const Loading = styled.div`
-display: flex,
-justify-content: center,
+  text-align: center;
+`;
 
+const Navigator = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
+const NavigatorActions = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border-radius: 10px;
+  width: 250px;
+  background-color: white;
+  border: 1px solid #cccccc;
+  margin: 0 0 10px;
+  box-shadow: 1px 8px 11px -6px rgba(82, 82, 82, 0.75);
+  -webkit-box-shadow: 1px 8px 11px -6px rgba(82, 82, 82, 0.75);
+  -moz-box-shadow: 1px 8px 11px -6px rgba(82, 82, 82, 0.75);
+
+  @media (max-width: 540px) {
+    width: 95%;
+  }
+`;
+
+const CurrentPage = styled.div`
+  font-size: 20px;
+  width: 80px;
+  text-align: center;
+  padding: 5px 0;
+  border-right: 1px solid #cccccc;
+  border-left: 1px solid #cccccc;
+
+  @media (max-width: 540px) {
+    width: 130px;
+  }
+`;
+
+const Previous = styled.div`
+  text-align: center;
+  padding: 5px 0;
+  transition: transform 80ms ease-in;
+
+  :active {
+    transform: scale(0.95);
+  }
+`;
+
+const Next = styled.div`
+  text-align: center;
+  padding: 5px 0;
+  transition: transform 80ms ease-in;
+
+  :active {
+    transform: scale(0.95);
+  }
 `;
 
 const Products = () => {
@@ -26,6 +81,20 @@ const Products = () => {
   var formData = new FormData();
 
   formData.set("offset", items);
+
+  function Increment() {
+    setItems((c) => c + 8);
+    setPages((c) => c + 1);
+  }
+
+  function Decrement() {
+    if (page <= 1) {
+      return;
+    } else {
+      setItems((c) => c - 8);
+      setPages((c) => c - 1);
+    }
+  }
 
   const { status, data, error, isFetching, isPreviousData } = useQuery(
     ["projects", items, itemsGet],
@@ -64,33 +133,47 @@ const Products = () => {
         )}
       </div>
 
-      <div> {isFetching ? null : <span> Current Page: {page}</span>}</div>
+      {/* <Loading>
+        {isFetching ? null : <span> Current Page: {page}</span>}
+      </Loading> */}
 
-      <div>
-        {isFetching ? null : (
-          <div>
-            <button
-              onClick={() => {
-                setItems((c) => c - 8);
-                setPages((c) => c - 1);
-              }}
-              disabled={items === 0}
-            >
-              Previous Page
-            </button>
-            <button
-              onClick={() => {
-                setItems((c) => c + 8);
-                setPages((c) => c + 1);
-              }}
-            >
-              Next Page
-            </button>
-          </div>
-        )}
-      </div>
+      <Loading>{isFetching ? <span> Loading...</span> : null}</Loading>
 
-      <div> {isFetching ? <span> Loading...</span> : null}</div>
+      <Navigator>
+        <NavigatorActions>
+          <Previous onClick={Decrement}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={25}
+              height={25}
+              fill="currentColor"
+              className="bi bi-chevron-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+          </Previous>
+          <CurrentPage>{page}</CurrentPage>
+          <Next onClick={Increment}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={25}
+              height={25}
+              fill="currentColor"
+              className="bi bi-chevron-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </Next>
+        </NavigatorActions>
+      </Navigator>
     </>
   );
 };
