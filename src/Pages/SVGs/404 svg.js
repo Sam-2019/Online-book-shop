b25ca9e4edfcbb86de596bf8a37fb07daa8638e0
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-25 13:01:01 +0000

    added 404 svg

---
+++ src/Pages/SVGs/404 svg.js
@@ -0,0 +1,101 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import { SvgItem } from "./SVGcontainer";
+
+const ComponentComponent = (props) => (
+  <SvgItem>
+    <svg {...props} width={244} height={244} viewBox="0 0 244 244">
+      <defs>
+        <style>
+          {
+            ".a{fill:#e3e3e3;}.b{fill:#646363;}.c{fill:#f1f3f7;}.d{fill:#838484;}.e{fill:#dededf;}.f{fill:#333842;}.g{fill:#a8a8a8;}"
+          }
+        </style>
+      </defs>
+      <circle
+        className="a"
+        cx={122}
+        cy={122}
+        r={122}
+        transform="translate(0 0)"
+      />
+      <rect
+        className="b"
+        width={173.813}
+        height={25.568}
+        transform="translate(35.093 47.767)"
+      />
+      <rect
+        className="c"
+        width={173.813}
+        height={126.837}
+        transform="translate(35.093 70.645)"
+      />
+      <circle
+        className="d"
+        cx={4.011}
+        cy={4.011}
+        r={4.011}
+        transform="translate(43.495 56.317)"
+      />
+      <circle
+        className="d"
+        cx={4.011}
+        cy={4.011}
+        r={4.011}
+        transform="translate(56.803 56.317)"
+      />
+      <circle
+        className="d"
+        cx={4.011}
+        cy={4.011}
+        r={4.011}
+        transform="translate(70.274 56.317)"
+      />
+      <g transform="translate(83.458 55.329)">
+        <path
+          className="e"
+          d="M267.422,118H182.363a4.011,4.011,0,1,0,0,8.022h85.059a4.067,4.067,0,0,0,4.011-4.011A3.862,3.862,0,0,0,267.422,118Z"
+          transform="translate(-178.352 -116.827)"
+        />
+        <circle
+          className="e"
+          cx={5.013}
+          cy={5.013}
+          r={5.013}
+          transform="translate(102.439)"
+        />
+      </g>
+      <path
+        className="f"
+        d="M395.925,122.114l-2.34-2.34a.827.827,0,1,0-1.17,1.17l1,.835h-4.345a.835.835,0,1,0,0,1.671h4.345l-1,1a.808.808,0,0,0,0,1.17,1,1,0,0,0,1.17,0l2.34-2.34c.167-.167.167-.334.167-.668C396.26,122.448,396.092,122.281,395.925,122.114Z"
+        transform="translate(-202.407 -62.302)"
+      />
+      <g transform="translate(58.879 111.708)">
+        <path
+          className="g"
+          d="M230.072,279.758a22.56,22.56,0,1,1,22.56-22.56A22.681,22.681,0,0,1,230.072,279.758Zm0-37.433A14.873,14.873,0,1,0,244.945,257.2,14.905,14.905,0,0,0,230.072,242.325Z"
+          transform="translate(-167.626 -234.638)"
+        />
+        <path
+          className="g"
+          d="M152.263,279.742a3.352,3.352,0,0,1-3.342-3.342v-7.353h-19.05a3.327,3.327,0,0,1-3.008-1.838,3.363,3.363,0,0,1,.5-3.51l22.393-26.9a3.354,3.354,0,0,1,3.677-1,3.211,3.211,0,0,1,2.172,3.008v23.729h4.011a3.342,3.342,0,0,1,0,6.684h-4.011v7.353A3.323,3.323,0,0,1,152.263,279.742Zm-15.374-17.213h12.2V247.991Z"
+          transform="translate(-126.581 -235.123)"
+        />
+        <path
+          className="g"
+          d="M334.857,279.742a3.352,3.352,0,0,1-3.342-3.342v-7.353H312.632a3.327,3.327,0,0,1-3.008-1.838,3.363,3.363,0,0,1,.5-3.51l22.393-26.9a3.354,3.354,0,0,1,3.677-1,3.212,3.212,0,0,1,2.172,3.008v23.729h4.011a3.342,3.342,0,0,1,0,6.684h-4.011v7.353A3.607,3.607,0,0,1,334.857,279.742Zm-15.374-17.213h12.2V247.991Z"
+          transform="translate(-219.269 -235.123)"
+        />
+      </g>
+    </svg>
+  </SvgItem>
+);
+
+export default ComponentComponent;
