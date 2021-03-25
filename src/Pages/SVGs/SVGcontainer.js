51a3db5632ca60aca61abb1d4d13c21cb0193cba
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-25 03:21:55 +0000

    added styled-components

--- src/Pages/SVGs/SVGcontainer.js
+++ src/Pages/SVGs/SVGcontainer.js
@@ -1,8 +1,31 @@
 import React from "react";
-import "./svgcontainer.css";
+import styled from "styled-components";
+
+const SvgPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 10% 0 0;
+  width: 35%;
+  margin: 0 auto;
+
+  @media (max-width: 540px) {
+    width: 60%;
+    padding: 30% 0 0;
+  }
+
+  @media (max-width: 530px) {
+    width: 90%;
+    padding: 30% 0 0;
+  }
+`;
+
+export const SvgItem = styled.div`
+  text-align: center;
+`;
 
 const SVGcontainer = ({ children }) => {
-  return <div className="svg-page">{children}</div>;
+  return <SvgPage>{children}</SvgPage>;
 };
 
 export default SVGcontainer;
