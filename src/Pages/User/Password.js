1231185fa7a0e74144c86e6a1e52858a616b0c82
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-25 20:09:28 +0000

    added New Passsword func w-i-p

--- src/Pages/User/Password.js
+++ src/Pages/User/Password.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Back from "../Components/Back";
+import { useHistory } from "react-router-dom";
 import { Input } from "../Components/Input";
 import Button from "../Components/Button";
 import Message from "../Components/Message";
 import { EyeShow, EyeHide } from "../Components/Eye";
+import { useData } from "../Context";
 import "./user.css";
 
 const Password = () => {
+  const { updateUserPassword, email } = useData();
+  const [loading, setLoading] = useState(false);
+
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
   const [show, hide] = React.useState("password");
 
+  let history = useHistory;
   let type;
 
   switch (show) {
@@ -19,6 +30,37 @@ const Password = () => {
     default:
       type = "password";
   }
+
+  const updatePassword = async (event) => {
+    setMessage("");
+    event.preventDefault();
+    var formData = new FormData();
+
+    let empty = newPassword && confirmPassword;
+
+    if (empty !== "") {
+      setLoading(true);
+      formData.set("buyer_unique_id", email);
+      formData.set("new_password", newPassword);
+      formData.set("confirm_password", confirmPassword);
+
+      const data = await updateUserPassword(formData);
+      console.log(data);
+
+      if (data.error === true) {
+        setMessage(data.message);
+        setError(data.error);
+        setLoading(false);
+      } else if (data.error === false) {
+        setNewPassword("");
+        setConfirmPassword("");
+        localStorage.removeItem("email");
+        setLoading(false);
+      }
+    } else if (empty === "") {
+      setMessage("Please fill the form");
+    } else return;
+  };
 
   return (
     <div className="user-wrapper">
@@ -44,13 +86,13 @@ const Password = () => {
           />
           <div className="eyeIcon">
             {show === "password" ? (
-                  <EyeHide
+              <EyeHide
                 action={() => {
                   hide("text");
                 }}
               />
             ) : (
-          <EyeShow
+              <EyeShow
                 action={() => {
                   hide("password");
                 }}
@@ -65,9 +107,25 @@ const Password = () => {
             type={type}
           />
 
-          <Message class_name="message " message="Hello" />
+          {message ? <Message class_name="message " message="Hello" /> : null}
 
-          <Button name="Submit" class_name="primary" />
+          {error ? (
+            <Button
+              name="Login"
+              action={() => {
+                history.push("/login");
+              }}
+              class_name="primary"
+            />
+          ) : (
+            <Button
+              name="Submit"
+              class_name="primary"
+              action={updatePassword}
+              loading={loading}
+            />
+          )}
+
           {/* <Button name="Signup" class_name="secondary" /> */}
         </form>
       </div>
