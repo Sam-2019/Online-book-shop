78ffd2d3626a12e447d229909b1b6dfb989e6eca
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-25 02:31:26 +0000

    renamed file

---
+++ src/Pages/SVGs/empty-orderhistory.js
@@ -0,0 +1,175 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const EmptyOrderHistory = (props) => (
+  <div className="svg-item">
+    <svg {...props} width={244} height={244} viewBox="0 0 244 244">
+      <defs>
+        <style>
+          {
+            ".a{fill:#e3e3e3;}.b{fill:#646363;}.c{fill:#c5c5c5;}.d{fill:#aaa;}.e{fill:#238c7c;}.f{fill:#838484;}.g{fill:#a8a8a8;}"
+          }
+        </style>
+      </defs>
+      <g transform="translate(-61 327)">
+        <g transform="translate(61 -327)">
+          <circle className="a" cx={122} cy={122} r={122} />
+          <path
+            className="b"
+            d="M106.736,151.625c0-4.194,4.034-7.625,8.227-7.625H233.151c4.194,0,7.023,3.431,7.023,7.625v19.063H106.736Z"
+            transform="translate(-55.87 -75.375)"
+          />
+          <path
+            className="c"
+            d="M0,0H114.372V64.812H0Z"
+            transform="translate(52.949 110.919) rotate(-45)"
+          />
+          <path
+            className="d"
+            d="M198.348,84.7l-65.64,65.64a11.459,11.459,0,0,1,0,9.844L163.3,190.781a11.459,11.459,0,0,1,9.844,0l65.64-65.64a11.459,11.459,0,0,1,0-9.844L208.192,84.7A11.446,11.446,0,0,1,198.348,84.7Z"
+            transform="translate(-69.464 -44.337)"
+          />
+          <g transform="translate(83.606 60.729)">
+            <circle
+              className="e"
+              cx={5.719}
+              cy={5.719}
+              r={5.719}
+              transform="translate(0 53.913)"
+            />
+            <circle
+              className="f"
+              cx={5.719}
+              cy={5.719}
+              r={5.719}
+              transform="translate(53.911)"
+            />
+            <circle
+              className="f"
+              cx={19.5}
+              cy={19.5}
+              r={19.5}
+              transform="translate(13.394 13.271)"
+            />
+          </g>
+          <path
+            className="g"
+            d="M106.736,160c0,4.194,4.034,7.625,8.227,7.625H242.682c4.194,0,7.023,3.431,7.023,7.625v87.688c0,4.194-2.825,7.625-7.023,7.625H114.963c-4.194,0-8.227-3.431-8.227-7.625Z"
+            transform="translate(-55.87 -83.75)"
+          />
+          <path
+            className="g"
+            d="M398.361,179.813A3.814,3.814,0,0,0,394.548,176h0a3.814,3.814,0,0,0-3.812,3.813v95.313a3.814,3.814,0,0,0,3.813,3.813h0a3.814,3.814,0,0,0,3.813-3.812Z"
+            transform="translate(-204.526 -92.125)"
+          />
+          <path
+            className="f"
+            d="M359.844,248H335.063a19.063,19.063,0,0,0,0,38.125h24.781c1.054,0,1.3-.854,1.3-1.906V249.906C361.148,248.854,360.9,248,359.844,248Z"
+            transform="translate(-165.406 -129.813)"
+          />
+          <path
+            className="g"
+            d="M335.085,252h-.3a17.3,17.3,0,0,0-.074,34.313H361.17V252Z"
+            transform="translate(-167.335 -131.906)"
+          />
+          <circle
+            className="b"
+            cx={8}
+            cy={8}
+            r={8}
+            transform="translate(160 129)"
+          />
+          <g transform="translate(58.491 95.313)">
+            <rect className="b" width={7.625} height={1.906} />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(19.062)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(38.125)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(57.187)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(76.242)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(95.305)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(114.367)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(0 81.969)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(19.062 81.969)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(38.125 81.969)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(57.187 81.969)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(76.242 81.969)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(95.305 81.969)"
+            />
+            <rect
+              className="b"
+              width={7.625}
+              height={1.906}
+              transform="translate(114.367 81.969)"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  </div>
+);
+
+export default EmptyOrderHistory;
