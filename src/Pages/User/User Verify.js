7986472f3b64c5869fb3ef8dda94f2b9fc172186
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-25 20:10:36 +0000

    added code for user verification #wip

--- src/Pages/User/User Verify.js
+++ src/Pages/User/User Verify.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import Back from "../Components/Back";
 import Message from "../Components/Message";
 import Success from "../Components/Success";
+import { useData } from "../Context";
+import { useAsync2 } from "../helper";
 import "./user.css";
 
 const UserVerify = () => {
+  const { verifyReadEmail } = useData();
+  const [message, setMessage] = useState("");
+
+  let token = new URLSearchParams(useLocation().search).get("token");
+  let email = new URLSearchParams(useLocation().search).get("email");
+
+  var formData = new FormData();
+  formData.set("url_data", token);
+  formData.set("url_data", email);
+
+  const resource = useAsync2(verifyReadEmail, formData);
+
+  if (resource.error === true) {
+    setMessage(resource.message);
+  } else if (resource.error === false) {
+    setMessage(resource.message);
+  }
+
   return (
     <div className="user-wrapper">
       <div className="header ">
@@ -23,9 +44,8 @@ const UserVerify = () => {
       </div>
 
       <div className="main">
+        {message ? <Message class_name="message " message={message} /> : null}
 
-      <Message class_name="message " message="Hello" />
-      
         <Success />
       </div>
     </div>
