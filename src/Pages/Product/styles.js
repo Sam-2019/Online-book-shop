3f8ebca90d263ad6c9b6f4b7e057200d9fc76ae6
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-28 19:56:05 +0000

    created styles component

---
+++ src/Pages/Product/styles.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Loading = styled.div`
+  text-align: center;
+  padding: 10px 0;
+`;
+
+export const Navigator = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const NavigatorActions = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border-radius: 10px;
+  width: 250px;
+  background-color: white;
+  border: 1px solid #cccccc;
+  paddding: 0 0 10px;
+  box-shadow: 1px 8px 11px -6px rgba(82, 82, 82, 0.75);
+  -webkit-box-shadow: 1px 8px 11px -6px rgba(82, 82, 82, 0.75);
+  -moz-box-shadow: 1px 8px 11px -6px rgba(82, 82, 82, 0.75);
+
+  @media (max-width: 540px) {
+    width: 95%;
+  }
+`;
+
+export const CurrentPage = styled.div`
+  font-size: 20px;
+  width: 80px;
+  text-align: center;
+  padding: 5px 0;
+  border-right: 1px solid #cccccc;
+  border-left: 1px solid #cccccc;
+
+  @media (max-width: 540px) {
+    width: 130px;
+  }
+`;
+
+export const Previous = styled.div`
+  text-align: center;
+  padding: 5px 0;
+  transition: transform 80ms ease-in;
+
+  :active {
+    transform: scale(0.95);
+  }
+`;
+
+export const Next = styled.div`
+  text-align: center;
+  padding: 5px 0;
+  transition: transform 80ms ease-in;
+
+  :active {
+    transform: scale(0.95);
+  }
+`;
+
