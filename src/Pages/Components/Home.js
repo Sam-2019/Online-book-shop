3d8f0c95aa759c5f846003eab412d0c3e494010d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-29 16:25:43 +0000

    added onClick event

--- src/Pages/Components/Home.js
+++ src/Pages/Components/Home.js
@@ -1,20 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 import "./icons.css";
 
-const Close = ({ width, height, action }) => {
+const Close = ({ width, height }) => {
+  let history = useHistory();
   return (
     <svg
       fill="currentColor"
-      class="bi bi-house-door" 
+      className="bi bi-house-door"
       viewBox="0 0 16 16"
       id="house"
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      onClick={action}
+      onClick={() => {
+        history.push("/");
+      }}
     >
- <path d="M8.354 1.146a.5.5 0 00-.708 0l-6 6A.5.5 0 001.5 7.5v7a.5.5 0 00.5.5h4.5a.5.5 0 00.5-.5v-4h2v4a.5.5 0 00.5.5H14a.5.5 0 00.5-.5v-7a.5.5 0 00-.146-.354L13 5.793V2.5a.5.5 0 00-.5-.5h-1a.5.5 0 00-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 00-.5-.5h-3a.5.5 0 00-.5.5v4H2.5z"/>
+      <path d="M8.354 1.146a.5.5 0 00-.708 0l-6 6A.5.5 0 001.5 7.5v7a.5.5 0 00.5.5h4.5a.5.5 0 00.5-.5v-4h2v4a.5.5 0 00.5.5H14a.5.5 0 00.5-.5v-7a.5.5 0 00-.146-.354L13 5.793V2.5a.5.5 0 00-.5-.5h-1a.5.5 0 00-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 00-.5-.5h-3a.5.5 0 00-.5.5v4H2.5z" />
     </svg>
   );
 };
