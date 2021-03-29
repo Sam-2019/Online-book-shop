04446c9ee38dc58e6c930ea552abddfc6119558d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-29 20:29:17 +0000

    fixed wide  view issue

--- src/Pages/Search/searchData.js
+++ src/Pages/Search/searchData.js
@@ -3,7 +3,7 @@ import SearchItem from "./searchItem";
 
 const SearchData = ({ data }) => {
   return (
-    <div className="products">
+    <div className="products2">
       {data.map((items, index) => (
         <SearchItem key={index} {...items} />
       ))}
