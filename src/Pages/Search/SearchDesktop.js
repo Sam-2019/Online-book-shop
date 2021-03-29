0e64932eba7a5b5ef9047303cf7ac80d3d9542a9
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-29 20:28:47 +0000

    refactored switch statement

--- src/Pages/Search/SearchDesktop.js
+++ src/Pages/Search/SearchDesktop.js
@@ -11,25 +11,32 @@ const Search = () => {
   let activePage;
   const desktopQuery = new URLSearchParams(useLocation().search).get("q");
 
-  const result = useAsync2("search_phrase", itemSearch, desktopQuery);
-  console.log(result.loading);
+  //const formData = React.useMemo(() => new FormData(), []);
 
-  switch (result.message) {
-    case "no results found":
-      activePage = <NoResult />;
-      break;
-    case "results found":
-      activePage = <SearchData data={result.value} />;
-      break;
-    default:
-      activePage = <Placeholder />;
-  }
+  const result = useAsync2("search_phrase", itemSearch, desktopQuery);
+
+  // switch (result.message) {
+  //   case "no results found":
+  //     activePage = <NoResult />;
+  //     break;
+  //   case "results found":
+  //     activePage = <SearchData data={result.value} />;
+  //     break;
+  //   default:
+  //     activePage = <Placeholder />;
+  // }
 
   return (
     <div className="search-wrapper ">
       <div className="main">
         <div className="title-makeshift ">
-          {result.loading ? <Placeholder /> : <>{activePage}</>}
+          {result.loading ? <Placeholder /> : null}
+
+          {result.message === "no results found" ? <NoResult /> : null}
+
+          {result.message === "results found" ? (
+            <SearchData data={result.value} />
+          ) : null}
         </div>
       </div>
     </div>
