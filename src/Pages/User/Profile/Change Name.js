8e9596abf21766890e9ed0984372328951f7d5b1
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-29 16:04:55 +0000

    added useMutation, fetch, update funcs, input values and state

--- src/Pages/User/Profile/Change Name.js
+++ src/Pages/User/Profile/Change Name.js
@@ -1,22 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
 import { Input } from "../../Components/Input";
 import Button from "../../Components/Button";
 import Message from "../../Components/Message";
-import "./change.css";
+import { userProfileUpdate } from "../../endpoints";
 import ProfilePhotoUpdate from "../../Components/Profile Photo Update ";
+import { useData } from "../../Context";
+
+import "./change.css";
 
 const ChangeName = ({ close }) => {
+  const { uniqueID } = useData();
+
+  const [loading, setLoading] = useState(false);
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const clear = () => {
+    setFirstName("");
+    setLastName("");
+  };
+
+  const mutation = useMutation((formData) => {
+    return fetch(userProfileUpdate, formData);
+  });
+
+  const updateDetail = async (event) => {
+    setMessage("");
+    event.preventDefault();
+
+    var formData = new FormData();
+
+    let empty = first_name && last_name;
+
+    if (empty === "") {
+      setMessage("Please fill the form");
+    }
+
+    if (empty !== "") {
+      setLoading(true);
+      formData.set("buyer_unique_id", uniqueID);
+      formData.set("firstname", first_name);
+      formData.set("lastname", last_name);
+
+      try {
+        const data = await mutation.mutateAsync(formData);
+        setMessage(data.message);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        clear();
+      }
+    }
+  };
+
   return (
     <>
       <ProfilePhotoUpdate />
 
       <form className=" change ">
-        <Input class_name="input " placeholder="First Name" onChange />
-        <Input class_name="input " placeholder="Last Name" onChange />
+        <Input
+          class_name="input "
+          placeholder="First Name"
+          value={first_name}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input
+          class_name="input "
+          placeholder="Last Name"
+          value={last_name}
+          onChange={(e) => setLastName(e.target.value)}
+        />
 
-        <Message class_name="message " message="Hello" />
+        {message ? <Message message={message} class_name="message" /> : null}
 
-        <Button class_name="primary" name="Update" />
+        <Button
+          class_name="primary"
+          name="Update"
+          action={updateDetail}
+          loading={loading}
+        />
 
         {/* <Button class_name="secondary" name="Cancel" action={close} /> */}
       </form>
--- src/Pages/User/Profile/Change Password.js
+++ src/Pages/User/Profile/Change Password.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
 import { Input } from "../../Components/Input";
 import Button from "../../Components/Button";
 import Message from "../../Components/Message";
 import { EyeShow, EyeHide } from "../../Components/Eye";
+import { userPasswordUpdate } from "../../endpoints";
+import Data from "../../Data";
 import "./change.css";
 
 const ChangePassword = ({ close }) => {
+  const { uniqueID, email } = Data();
+
   const [show, hide] = React.useState("password");
+
+  const [loading, setLoading] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   let type;
 
@@ -18,28 +29,69 @@ const ChangePassword = ({ close }) => {
       type = "password";
   }
 
+  const mutation = useMutation((formData) => {
+    return fetch(userPasswordUpdate, formData);
+  });
+
+  const reset = () => {
+    setCurrentPassword("");
+    setNewPassword("");
+    setConfirmPassword("");
+  };
+
+  const updatePassword = async (event) => {
+    setMessage();
+    event.preventDefault();
+    var formData = new FormData();
+
+    let empty = currentPassword && newPassword && confirmPassword;
+
+    if (empty === "") {
+      setMessage("Please fill the form");
+    }
+
+    if (empty !== "") {
+      setLoading(true);
+      formData.set("buyer_unique_id", uniqueID);
+      formData.set("current_password", currentPassword);
+      formData.set("new_password", newPassword);
+      formData.set("confirm_password", confirmPassword);
+
+      try {
+        const data = await mutation.mutateAsync(formData);
+        setMessage(data.message);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        reset();
+      }
+    }
+  };
+
   return (
     <form className=" change ">
-      <p>KenLay@enron-fraud.com</p>
+      <p>{email}</p>
 
       <Input
         class_name="input "
-        placeholder="New Password"
         type={type}
-        onChange
+        placeholder="Current Password"
+        value={currentPassword}
+        onChange={(e) => setCurrentPassword(e.target.value)}
       />
 
       <div className="eyeIcon2">
         {show === "password" ? (
-          <EyeShow
-            action={() => {
-              hide("text");
-            }}
-          />
-        ) : (
           <EyeHide
             action={() => {
               hide("password");
+            }}
+          />
+        ) : (
+          <EyeShow
+            action={() => {
+              hide("text");
             }}
           />
         )}
@@ -48,13 +100,27 @@ const ChangePassword = ({ close }) => {
       <Input
         class_name="input "
         type={type}
-        placeholder="Confirm New Password"
-        onChange
+        placeholder="New Password"
+        value={newPassword}
+        onChange={(e) => setNewPassword(e.target.value)}
       />
 
-      <Message class_name="message " message="Hello" />
+      <Input
+        class_name="input "
+        type={type}
+        placeholder="Confirm New Password"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
+      />
 
-      <Button class_name="primary" name="Update" />
+      {message ? <Message message={message} class_name="message" /> : null}
+
+      <Button
+        class_name="primary"
+        name="Update"
+        action={updatePassword}
+        loading={loading}
+      />
 
       <Button class_name="secondary" name="Cancel" action={close} />
     </form>
