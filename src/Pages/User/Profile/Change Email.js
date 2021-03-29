54d66c5eb7f48eae18a87878a8d53689d11477f2
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-29 16:03:21 +0000

    added useMutation, fetch, update funcs, input values and state

--- src/Pages/User/Profile/Change Email.js
+++ src/Pages/User/Profile/Change Email.js
@@ -1,18 +1,82 @@
-import React from "react";
-import {Input} from "../../Components/Input";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
+import { Input } from "../../Components/Input";
 import Button from "../../Components/Button";
 import Message from "../../Components/Message";
+import Data from "../../Data";
+import { userEmailUpdate } from "../../endpoints";
+import { useData } from "../../Context";
 import "./change.css";
 
 const ChangeEmail = ({ close }) => {
+  const { uniqueID } = useData();
+  const [loading, setLoading] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [new_email, setNewEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const reset = () => {
+    setEmail("");
+    setNewEmail("");
+  };
+
+  const mutation = useMutation((formData) => {
+    return fetch(userEmailUpdate, formData);
+  });
+
+  const updateEmail = async (event) => {
+    event.preventDefault();
+    setMessage("");
+
+    var formData = new FormData();
+
+    let empty = email;
+
+    if (empty === "") {
+      setMessage("Please fill the form");
+    }
+
+    if (empty !== "") {
+      setLoading(true);
+      formData.set("buyer_unique_id", uniqueID);
+      formData.set("email", email);
+
+      try {
+        const data = await mutation.mutateAsync(formData);
+        setMessage(data.message);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        reset();
+      }
+    }
+  };
+
   return (
     <form className=" change ">
-      <Input class_name="input " placeholder="Current Email" onChange />
-      <Input class_name="input " placeholder="New Email" onChange />
+      <Input
+        class_name="input "
+        placeholder="Current Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        class_name="input "
+        placeholder="New Email"
+        value={new_email}
+        onChange={(e) => setNewEmail(e.target.value)}
+      />
 
-      <Message class_name="message " message="Hello" />
+      {message ? <Message message={message} class_name="message" /> : null}
 
-      <Button class_name="primary" name="Update" />
+      <Button
+        class_name="primary"
+        name="Update"
+        action={updateEmail}
+        loading={loading}
+      />
 
       <Button class_name="secondary" name="Cancel" action={close} />
     </form>
