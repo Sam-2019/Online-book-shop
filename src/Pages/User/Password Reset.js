02f12a8a8d6b99ab5b0925cdcbaf271d4324bfe3
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-29 16:09:50 +0000

    added useMutation, fetch, action funcs, input values and state

--- src/Pages/User/Password Reset.js
+++ src/Pages/User/Password Reset.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
 import Back from "../Components/Back";
-import {Input} from "../Components/Input";
+import { Input } from "../Components/Input";
 import Button from "../Components/Button";
 import Message from "../Components/Message";
+import { fetch } from "../helper";
+import { userAccountReset } from "../endpoints";
+
 import "./user.css";
 
 const PasswordReset = () => {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const mutation = useMutation((formData) => {
+    return fetch(userAccountReset, formData);
+  });
+
+  const clear = () => {
+    setEmail("");
+  };
+
+  const sendEmail = async (event) => {
+    event.preventDefault();
+
+    setMessage("");
+
+    let empty = email;
+
+    if (empty === "") {
+      setMessage("Please fill the form");
+    }
+
+    if (empty !== "") {
+      setLoading(true);
+
+      var formData = new FormData();
+      formData.set("buyer_email", email);
+
+      try {
+        const data = await mutation.mutateAsync(formData);
+        console.log(data);
+        setMessage(data.message);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        clear();
+      }
+    }
+  };
+
   return (
     <div className="user-wrapper">
       <div className="header ">
@@ -29,11 +75,22 @@ const PasswordReset = () => {
 
       <div className="main">
         <form className="form-wrapper outline login-box ">
-          <Input class_name="input " placeholder="Email" onChange />
+          <Input
+            class_name="input "
+            placeholder="Email"
+            value={email}
+            action={(e) => setEmail(e.target.value)}
+            type="email"
+          />
 
-          <Message class_name="message " message="Hello" />
+          {message ? <Message message={message} class_name="message" /> : null}
 
-          <Button name="Send password reset email" class_name="primary" />
+          <Button
+            name="Send email"
+            class_name="primary"
+            action={sendEmail}
+            loading={loading}
+          />
 
           {/* <Button name="Return to sign in" class_name="secondary" /> */}
           {/* <Button name="Signup" class_name="secondary" /> */}
