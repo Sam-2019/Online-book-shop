a15c40bee61cbcc133d783445c360b8c3555e9ac
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-30 23:08:31 +0000

    updated data source

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Back from "../Components/Back";
 import ReviewItem from "./reviewItem";
-import { data } from "./reviewData";
+import { reviewData } from "./reviewData";
 import "./review.css";
 
 const Review = () => {
@@ -12,13 +12,13 @@ const Review = () => {
           <div className="object-1">
             <Back width={30} height={30} />
           </div>
-          <div className="object-2">Review ({data.length})</div>
+          <div className="object-2">Review ({reviewData.length})</div>
         </div>
       </div>
 
       <div className="main">
         <div className=" wrapper-item">
-          {data.map((item, index) => (
+          {reviewData.map((item, index) => (
             <ReviewItem key={index} {...item} />
           ))}
         </div>
