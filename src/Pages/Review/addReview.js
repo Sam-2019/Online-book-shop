049fd38b0dd83027245e8b70b356d0e5cc2de17a
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-30 23:09:59 +0000

    addec submit func & state

--- src/Pages/Review/addReview.js
+++ src/Pages/Review/addReview.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextArea } from "../Components/Input";
 import Button from "../Components/Button";
 import Message from "../Components/Message";
-import StarRating from '../Components/StarRating'
+import StarRating from "../Components/StarRating";
 
 const AddReview = ({ close }) => {
+  const [text, setText] = useState("");
+  const [message, setMessage] = useState("");
+
+  function submit(e) {
+    e.preventDefault();
+
+    if (text === "") {
+      setMessage("Please fill the form");
+    }
+
+    if (text.length >= 50) {
+      close();
+    }
+  }
+
   return (
     <form className=" change ">
-    
+      <StarRating />
 
-      <StarRating  />
+      <TextArea
+        class_name="text-input "
+        placeholder="Leave a review"
+        action={(e) => setText(e.target.value)}
+        value={text}
+      />
 
-      <TextArea class_name="text-input " placeholder="Leave a review" onChange />
+      {message ? <Message class_name="message " message={message} /> : null}
 
-      <Button class_name="primary" name="Post" />
+      <Button class_name="primary" name="Post" action={submit} />
 
       <Button class_name="secondary" name="Cancel" action={close} />
     </form>
