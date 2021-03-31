52de6427372b9c973b41fec3cb6f79407709b272
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-31 19:22:40 +0000

    removed useState import

--- src/Pages/User/User Verify.js
+++ src/Pages/User/User Verify.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import Back from "../Components/Back";
 import Message from "../Components/Message";
