ba14708efcc094d781cb777a38d7c6af03a1a6c4
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-03-31 18:03:07 +0000

    added propType

--- src/Pages/Context.js
+++ src/Pages/Context.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext } from "react";
+import PropTypes from "prop-types";
 import Data from "./Data";
 
 export const Context = createContext();
@@ -13,3 +14,7 @@ export const ContextProvider = ({ children }) => {
 };
 
 export default Context;
+
+ContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
