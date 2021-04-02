409f3d3276aaea85d1d9d85394673d27a4042afd
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-02 08:51:34 +0000

    added verify func, styled-component

--- src/Pages/Components/Verify.js
+++ src/Pages/Components/Verify.js
@@ -1,17 +1,81 @@
 import React from "react";
+import { useMutation } from "react-query";
+import styled from "styled-components";
+import { toast } from "react-toastify";
 import { useData } from "../Context";
+import { userCreateEmailVerify } from "../endpoints";
+import { fetch } from "../helper";
+
+toast.configure();
+
+const Verification = styled.div`
+  font-size: 12px;
+  background-color: white;
+  font-style: italic;
+  display: flex;
+`;
+
+const Verified = styled.div`
+  border: 1px solid #00d919;
+  color: #00d919;
+  border-radius: 10px;
+  border-radius: 10px;
+  padding: 0 10px;
+`;
+
+const NotVerified = styled.div`
+  border: 1px solid #d91d00;
+  color: #d91d00;
+  border-radius: 10px;
+  padding: 0 10px;
+  cursor: pointer;
+
+  :active {
+    transform: scale(0.95);
+  }
+
+  :focus {
+    outline: none;
+  }
+`;
 
 const Verify = () => {
-  const { verfifcationStatus } = useData();
+  const { verfifcationStatus, email } = useData();
+
+  const notify = (data) => {
+    toast.success(data);
+  };
+
+  const mutation = useMutation((formData) => {
+    return fetch(userCreateEmailVerify, formData);
+  });
+
+  const verify = async (event) => {
+    var formData = new FormData();
+    event.preventDefault();
+    formData.set("buyer_email", email);
+
+    try {
+      const data = await mutation.mutateAsync(formData);
+      console.log(data);
+
+      if (data) {
+        notify(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+    }
+  };
 
   return (
-    <div className="user-verify">
-      {verfifcationStatus ? (
-        <span className="verify">Verified</span>
+    <Verification>
+      {verfifcationStatus !== "Verified" ? (
+        <Verified>Verified</Verified>
       ) : (
-        <span className="not-verify">Unverified</span>
+        <NotVerified onClick={verify}>Unverified</NotVerified>
       )}
-    </div>
+    </Verification>
   );
 };
 
