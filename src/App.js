2a145de4f3b18f03dc6ac698b3033f70c2105c3b
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-12 21:27:15 +0000

    imported @apollo/client

--- src/App.js
+++ src/App.js
@@ -1,38 +1,51 @@
 import React from "react";
 import styled from "styled-components";
-import Okukus from "./Pages/Okukus";
 import "bootstrap/dist/css/bootstrap.css";
-import { QueryClient, QueryClientProvider } from "react-query";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+} from "@apollo/client";
+
+import Okukus from "./Pages/Okukus";
 import { ContextProvider } from "./Pages/Context";
 import "./Pages/okukus.css";
-import "react-toastify/dist/ReactToastify.css";
 
 const AppBackgroundColour = styled.div`
   background: #ababab3c;
 `;
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-    },
-  },
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
 });
+
+// client
+//   .query({
+//     query: gql`
+//       query {
+//         products {
+//           id
+//           name
+//           sku
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result));
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <ApolloProvider client={client}>
       <AppBackgroundColour>
         <ContextProvider>
           <Okukus />
         </ContextProvider>
       </AppBackgroundColour>
-    </QueryClientProvider>
+    </ApolloProvider>
   );
 }
-
-// function App() {
-//   return <File />;
-// }
 
 export default App;
--- src/Pages/Product/Products.js
+++ src/Pages/Product/Products.js
@@ -1,11 +1,7 @@
 import React from "react";
-import { useQuery, QueryClient } from "react-query";
-//import Loader from "react-loaders";
+import { useQuery, gql } from "@apollo/client";
 import ProductsItem from "./productsItem";
 import Placeholder from "../Placeholders/Products";
-import { itemsGet } from "../endpoints";
-import Loader from "../Loader/loader";
-import { fetch } from "../helper";
 import {
   Loading,
   Navigator,
@@ -13,109 +9,85 @@ import {
   CurrentPage,
   Previous,
   Next,
+  Spacer,
 } from "../styles";
 import "./products.css";
 
-const queryClient = new QueryClient();
+const GET_PRODUCTS = gql`
+  query {
+    products {
+      id
+      name
+      sku
+      price
+      imageURL
+    }
+  }
+`;
 
 const Products = () => {
-  const [items, setItems] = React.useState(0);
-  const [page, setPages] = React.useState(1);
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
 
-  var formData = new FormData();
-  formData.set("offset", items);
+  if (loading)
+    return (
+      <Loading>
+        <Placeholder />
+      </Loading>
+    );
 
-  function Increment() {
-    setItems((c) => c + 8);
-    setPages((c) => c + 1);
-  }
-
-  function Decrement() {
-    if (page <= 1) {
-      return;
-    } else {
-      setItems((c) => c - 8);
-      setPages((c) => c - 1);
-    }
-  }
-
-  const { status, data, error, isFetching, isPreviousData } = useQuery(
-    ["products", items, itemsGet],
-    () => fetch(itemsGet, formData),
-    {
-      keepPreviousData: true,
-      staleTime: 5000,
-      cacheTime: 20000,
-    }
-  );
-
-  React.useEffect(() => {
-    if (data?.hasMore) {
-      queryClient.prefetchQuery(["projects", items + 1], () =>
-        fetch(items + 1)
-      );
-    }
-  }, [data, items]);
+  if (error)
+    return (
+      <Loading>
+        <p>Error :(</p>
+      </Loading>
+    );
 
   return (
-    <>
-      <div>
+    <Loading>
+      {data && (
+        <div className="products">
+          {data.products.map((items, index) => (
+            <ProductsItem key={index} {...items} />
+          ))}
+        </div>
+      )}
 
+      <Spacer />
 
-        {status === "loading" && <Placeholder />}
-
-        {status === "error" && <div>Error: {error.message}</div>}
-
-        {status === "success" && (
-          <div className="products">
-            {data.map((items, index) => (
-              <ProductsItem key={index} {...items} />
-            ))}
-          </div>
-        )}
-        
-      </div>
-
-      <Loading>
-        {isFetching ? (
-          <Loader />
-        ) : (
-          <Navigator>
-            <NavigatorActions>
-              <Previous onClick={Decrement}>
-                <svg
-                  width="24px"
-                  height="24px"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M16.2431 6.34317L14.8288 4.92896L7.75781 12L14.8289 19.0711L16.2431 17.6569L10.5862 12L16.2431 6.34317Z"
-                    fill="gray"
-                  />
-                </svg>
-              </Previous>
-              <CurrentPage>{page}</CurrentPage>
-              <Next onClick={Increment}>
-                <svg
-                  width="24px"
-                  height="24px"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10.5859 6.34314L12.0002 4.92892L19.0712 12L12.0002 19.0711L10.5859 17.6568L16.2428 12L10.5859 6.34314Z"
-                    fill="gray"
-                  />
-                </svg>
-              </Next>
-            </NavigatorActions>
-          </Navigator>
-        )}
-      </Loading>
-    </>
+      <Navigator>
+        <NavigatorActions>
+          <Previous>
+            <svg
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16.2431 6.34317L14.8288 4.92896L7.75781 12L14.8289 19.0711L16.2431 17.6569L10.5862 12L16.2431 6.34317Z"
+                fill="gray"
+              />
+            </svg>
+          </Previous>
+          <CurrentPage>1</CurrentPage>
+          <Next>
+            <svg
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.5859 6.34314L12.0002 4.92892L19.0712 12L12.0002 19.0711L10.5859 17.6568L16.2428 12L10.5859 6.34314Z"
+                fill="gray"
+              />
+            </svg>
+          </Next>
+        </NavigatorActions>
+      </Navigator>
+    </Loading>
   );
 };
 
