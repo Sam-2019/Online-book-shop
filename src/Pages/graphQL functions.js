d5c531b1ada9fca41df7267a75f9c033983b0c99
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-13 23:07:04 +0000

    created graphQL fns

---
+++ src/Pages/graphQL functions.js
@@ -0,0 +1,76 @@
+import { gql } from "@apollo/client";
+
+export const GET_CART = gql`
+  query Carts($id: ID!) {
+    carts(id: $id) {
+      id
+      product {
+        id
+        sku
+        name
+        author
+        price
+        imageURL
+        quantity
+      }
+    }
+  }
+`;
+
+export const GET_PRODUCT = gql`
+  query Product($sku: String!) {
+    product(sku: $sku) {
+      id
+      name
+      sku
+      author
+      price
+      imageURL
+      quantity
+      detail
+    }
+  }
+`;
+
+export const ADD_CART = gql`
+  mutation AddCart(
+    $user: ID!
+    $product: ID!
+    $quantity: String!
+    $price: String!
+  ) {
+    addCart(
+      user: $user
+      product: $product
+      quantity: $quantity
+      price: $price
+    ) {
+      id
+      user
+      price
+      quantity
+    }
+  }
+`;
+
+export const ADD_WISHLIST = gql`
+  mutation AddWishlist($user: ID!, $product: ID!) {
+    addWishlist(user: $user, product: $product) {
+      id
+      user
+      product
+    }
+  }
+`;
+
+export const GET_PRODUCTS = gql`
+query {
+  products {
+    id
+    name
+    sku
+    price
+    imageURL
+  }
+}
+`;
