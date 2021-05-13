36a6b0747ba71ee66ea9d1481b72000e7b2ba01b
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-13 23:08:53 +0000

    added products data

---
+++ src/Pages/jsdata.js
@@ -0,0 +1,134 @@
+export const products = [
+  {
+    _id: "6099a0f3c268590778baf757",
+    name: "The Spider Network: The Wild Story of a Math Genius, a Gang of Backstabbing Bankers, and One of the Greatest Scams in Financial History",
+    author: "David Enrich",
+    sku: "the spider network",
+    price: "28.75",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/I/51D8Gcl22UL._SX329_BO1,204,203,200_.jpg",
+    quantity: 5,
+    detail:
+      "In 2006, an oddball group of bankers, traders and brokers from some of the largest financial institutions made a startling realization: Libor—the London interbank offered rate, which determines the interest rates on trillions in loans worldwide—was set daily by a small group of easily manipulated administrators, and that they could reap huge profits by nudging it fractions of a percent to suit their trading portfolios. Tom Hayes, a brilliant but troubled mathematician, became the lynchpin of a wild alliance that included a prickly French trader nicknamed “Gollum”; the broker “Abbo,” who liked to publicly strip naked when drinking; a nervous Kazakh chicken farmer known as “Derka Derka”; a broker known as “Village” (short for “Village Idiot” who racked up huge expense account bills; an executive called “Clumpy” because of his patchwork hair loss; and a broker uncreatively nicknamed “Big Nose” who had once been a semi-professional boxer. This group generated incredible riches —until it all unraveled in spectacularly vicious, backstabbing fashion. With exclusive access to key characters and evidence, The Spider Network is not only a rollicking account of the scam, but also a provocative examination of a financial system that was crooked throughout.",
+  },
+
+  {
+    _id: "6099a320c268590778baf758",
+    name: "Antifragile: Things That Gain from Disorder",
+    author: "Nassim Nicholas Taleb ",
+    sku: "antifragile",
+    price: "12.82",
+    imageURL:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1352422827l/13530973.jpg",
+    quantity: 5,
+    detail:
+      "From the bestselling author of The Black Swan and one of the foremost philosophers of our time, Nassim Nicholas Taleb, a book on how some systems actually benefit from disorder.In The Black Swan Taleb outlined a problem; in Antifragile he offers a definitive solution: how to gain from disorder and chaos while being protected from fragilities and adverse events. For what he calls the 'antifragile' is one step beyond robust, as it benefits from adversity, uncertainty and stressors, just as human bones get stronger when subjected to stress and tension.Taleb stands uncertainty on its head, making it desirable, and proposing that things be built in an antifragile manner. Extremely ambitious and multidisciplinary, Antifragile provides a blueprint for how to behave-and thrive-in a world we don't understand and which is too uncertain for us to even try to understand. He who is not antifragile will perish. Why is the city state better than the nation state, why is debt bad for you, and why is almost everything modern bound to fail? The book covers innovation, health, biology, medicine, life decisions, politics, foreign policy, urban planning, war, personal finance, and economic systems. Throughout, the voice and recipes of the ancient wisdom from Phoenician, Roman, Greek, and Medieval sources are heard loud and clear.",
+  },
+  {
+    _id: "6099a3c9c268590778baf759",
+    name: "More Than You Know: Finding Financial Wisdom in Unconventional Places",
+    author: "Michael J. Mauboussin",
+    sku: "more than you know",
+    price: "24.06",
+    imageURL:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1386924121l/33331.jpg",
+    quantity: 5,
+    detail:
+      "Michael Mauboussin, one of Wall Street's most creative and influential minds offers provocative new ways of thinking about the stock market, investing, and how we make decisions.",
+  },
+  {
+    _id: "6099a48ac268590778baf75a",
+    name: "Expectations Investing: Reading Stock Prices for Better Returns",
+    author: "Alfred Rappaport, Michael J. Mauboussin, Peter L. Bernstein",
+    sku: "expectations investing",
+    price: "148.95",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/I/41t1esOD4+L._SX328_BO1,204,203,200_.jpg",
+    quantity: 5,
+    detail:
+      "Expectations Investing offers a unique and powerful alternative for identifying value-price gaps. Rappaport and Mauboussin provide everything the reader needs to utilize the discounted cash flow model successfully. And they add an important twist: they suggest that rather than forecasting cash flows, investors should begin by estimating the expectations embedded in a company's stock price. An investor who has a fix on the market's expectations can then assess the likelihood of expectations revisions. To help investors anticipate such revisions, Rappaport and Mauboussin introduce an 'expectations infrastructure' framework for tracing the process of value creation from the basic economic forces that shape a company's performance to the resulting impact on sales, costs, and investment. Investors who use Expectations Investing will have a fundamentally new way to evaluate all stocks, setting them on the path to success. Managers will be able to use the book to devise, adjust, and communicate their company's strategy in light of shareholder expectations.",
+  },
+  {
+    _id: "6099a5aac268590778baf75b",
+    name: "The Psychology of Money: Timeless Lessons on Wealth, Greed, and Happiness",
+    author: "Morgan Housel",
+    sku: "the psychology of money",
+    price: "16.24",
+    imageURL: "https://m.media-amazon.com/images/I/51jRBz6Ug3L.jpg",
+    quantity: 5,
+    detail:
+      "Doing well with money isn’t necessarily about what you know. It’s about how you behave. And behavior is hard to teach, even to really smart people. Money - investing, personal finance, and business decisions - is typically taught as a math-based field, where data and formulas tell us exactly what to do. But in the real world people don’t make financial decisions on a spreadsheet. They make them at the dinner table, or in a meeting room, where personal history, your own unique view of the world, ego, pride, marketing, and odd incentives are scrambled together.   In The Psychology of Money, award-winning author Morgan Housel shares 19 short stories exploring the strange ways people think about money and teaches you how to make better sense of one of life’s most important topics.",
+  },
+
+  {
+    _id: "6099aa566b1d082a00af22e3",
+    name: "Bailout Nation: How Greed and Easy Money Corrupted Wall Street and Shook the World Economy",
+    author: "Barry Ritholtz",
+    sku: "bailout nation",
+    price: "12.24",
+    imageURL: "https://m.media-amazon.com/images/I/51wdc9a2pJL.jpg",
+    quantity: 5,
+    detail:
+      "Bailout Nation offers one of the clearest looks at the financial lenders, regulators, and politicians responsible for the financial crisis of 2008. Written by Barry Ritholtz, one of the most popular economic bloggers and a well-established industry pundit, this book skillfully explores how the United States evolved from a rugged independent nation to a soft Bailout Nation - where financial firms are allowed to self-regulate in good times, but are bailed out by taxpayers in bad times.  Entertaining and informative, this book clearly shows you how years of trying to control the economy with easy money has finally caught up with the federal government and how its practice of repeatedly rescuing Wall Street has come back to bite them. *The definitive book on the financial crisis of 2008  *Names the villains responsible for this tragedy - from financial regulators to politicians  *Shows how each bailout throughout modern history has impacted what happened in the future *Examines why the consumer/taxpayer is left suffering in an economy of bubbles, bailouts, and possible inflation. Scathing, but fair, Bailout Nation is a voice of reason in uncertain economic times.",
+  },
+
+  {
+    _id: "6099ab216b1d082a00af22e4",
+    name: "The 48 Laws of Power",
+    author: "Robert Greene",
+    sku: "48 laws of power",
+    price: "14.50",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/I/41JIh4KMHRL._SX355_BO1,204,203,200_.jpg",
+    quantity: 5,
+    detail:
+      "In the book that People magazine proclaimed “beguiling” and “fascinating,” Robert Greene and Joost Elffers have distilled three thousand years of the history of power into 48 essential laws by drawing from the philosophies of Machiavelli, Sun Tzu, and Carl Von Clausewitz and also from the lives of figures ranging from Henry Kissinger to P.T. Barnum. Some laws teach the need for prudence (“Law 1: Never Outshine the Master”, others teach the value of confidence (“Law 28: Enter Action with Boldness”, and many recommend absolute self-preservation (“Law 15: Crush Your Enemy Totally”. Every law, though, has one thing in common: an interest in total domination. In a bold and arresting two-color package, The 48 Laws of Power is ideal whether your aim is conquest, self-defense, or simply to understand the rules of the game.",
+  },
+  {
+    _id: "6099afe16b1d082a00af22e5",
+    name: "Skin in the Game: Hidden Asymmetries in Daily Life",
+    author: "Nassim Nicholas Taleb",
+    sku: "skin in the game",
+    price: "14.85",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/I/41gv2dXzILL._SX314_BO1,204,203,200_.jpg",
+    quantity: 5,
+    detail:
+      "In his most provocative and practical book yet, one of the foremost thinkers of our time redefines what it means to understand the world, succeed in a profession, contribute to a fair and just society, detect nonsense, and influence others. Citing examples ranging from Hammurabi to Seneca, Antaeus the Giant to Donald Trump, Nassim Nicholas Taleb shows how the willingness to accept one’s own risks is an essential attribute of heroes, saints, and flourishing people in all walks of life.",
+  },
+  {
+    _id: "6099b07c6b1d082a00af22e6",
+    name: "Misbehaving: The Making of Behavioral Economics",
+    author: "Richard H. Thaler",
+    sku: "misbehaving",
+    price: "13.71",
+    imageURL: "https://m.media-amazon.com/images/I/51LsfuzFU7L.jpg",
+    quantity: 5,
+    detail:
+      "Richard H. Thaler has spent his career studying the radical notion that the central agents in the economy are humans - predictable, error-prone individuals. Misbehaving is his arresting, frequently hilarious account of the struggle to bring an academic discipline back down to earth - and change the way we think about economics, ourselves, and our world.Traditional economics assumes rational actors. Early in his research, Thaler realized these Spock-like automatons were nothing like real people. Whether buying a clock radio, selling basketball tickets, or applying for a mortgage, we all succumb to biases and make decisions that deviate from the standards of rationality assumed by economists. In other words we misbehave. More importantly, our misbehavior has serious consequences. Dismissed at first by economists as an amusing sideshow, the study of human miscalculations and their effects on markets now drives efforts to make better decisions in our lives, our businesses, and our governments.Coupling recent discoveries in human psychology with a practical understanding of incentives and market behavior, Thaler enlightens listeners about how to make smarter decisions in an increasingly mystifying world. He reveals how behavioral economic analysis opens up new ways to look at everything from household finance to assigning faculty offices in a new building to TV game shows, the NFL draft, and businesses like Uber.Laced with antic stories of Thaler's spirited battles with the bastions of traditional economic thinking, Misbehaving is a singular look into profound human foibles. When economics meets psychology, the implications for individuals, managers, and policy makers are both profound and entertaining.",
+  },
+
+  {
+    _id: "6099b2596b1d082a00af22e7",
+    name: "Thinking, Fast and Slow",
+    author: "Daniel Kahneman",
+    sku: "thinking fast and slow",
+    price: "17.41",
+    imageURL: "https://m.media-amazon.com/images/I/4150eh0V-rL.jpg",
+    quantity: 5,
+    detail:
+      "The guru to the gurus at last shares his knowledge with the rest of us. Nobel laureate Daniel Kahneman's seminal studies in behavioral psychology, behavioral economics, and happiness studies have influenced numerous other authors, including Steven Pinker and Malcolm Gladwell. In Thinking, Fast and Slow, Kahneman at last offers his own, first book for the general public. It is a lucid and enlightening summary of his life's work. It will change the way you think about thinking. Two systems drive the way we think and make choices, Kahneman explains: System One is fast, intuitive, and emotional; System Two is slower, more deliberative, and more logical. Examining how both systems function within the mind, Kahneman exposes the extraordinary capabilities as well as the biases of fast thinking and the pervasive influence of intuitive impressions on our thoughts and our choices. Engaging the reader in a lively conversation about how we think, he shows where we can trust our intuitions and how we can tap into the benefits of slow thinking, contrasting the two-system view of the mind with the standard model of the rational economic agent. Kahneman's singularly influential work has transformed cognitive psychology and launched the new fields of behavioral economics and happiness studies. In this path-breaking book, Kahneman shows how the mind works, and offers practical and enlightening insights into how choices are made in both our business and personal lives - and how we can guard against the mental glitches that often get us into trouble. ",
+  },
+
+  {
+    _id: "609a7fdbbc7958071ca94fac",
+    name: "Flash Boys: A Wall Street Revolt",
+    author: "Michael Lewis",
+    sku: "flash boys",
+    price: "14.25",
+    imageURL: "https://m.media-amazon.com/images/I/51yotsewD6L.jpg",
+    quantity: 5,
+    detail:
+      "From the number-one New York Times best-selling author of Liar's Poker and 'one of the country's most popular business journalists' (The New York Times Michael Lewis, comes an engaging new book about Wall Street.  Michael Lewis, the best-selling author of Boomerang, The Big Short, The Blind Side, Moneyball, and many others, returns to the financial world to give listeners a ringside seat as the biggest news story in years prepares to hit Wall Street.",
+  },
+];
