ab5c1ea68030fe71f2ece054843e49ca063a4445
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-18 21:26:57 +0000

    imported useData

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,28 +1,34 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
 import { useHistory } from "react-router-dom";
-import { MediaQuery } from "../helper";
+
 import Back from "../Components/Back";
 import Button from "../Components/Button";
 import Summary from "../Summary/Summary";
 import CartData from "./cartData";
-import CartHeader from "./cartHeader";
+
 import "./cart.css";
 import { products } from "../jsdata";
 import { GET_CART } from "../graphQL functions";
 
 import SVGcontainer from "../SVGs/SVGcontainer";
 import EmptyCart from "../SVGs/empty-cart";
+import { useData } from "../Context";
 
 const Cart = () => {
-  const id = String("609bfb663aef9216e4528eed");
-  const breakpoint = 540;
-  const { width } = MediaQuery();
-  let history = useHistory();
+  const { uniqueID } = useData();
 
-  const { loading, error, data } = useQuery(GET_CART, {
+  const id = String(uniqueID);
+
+  console.log(id);
+
+  const { loading, error, data, refetch } = useQuery(GET_CART, {
     variables: { id },
   });
+
+  console.log(data);
+
+  let history = useHistory();
 
   const array = new Uint32Array(1);
   const index = window.crypto.getRandomValues(array);
@@ -30,6 +36,7 @@ const Cart = () => {
   function orderItem() {
     history.push(`/order/${index[0]}`);
   }
+
   return (
     <div className="cart-wrapper">
       <div className="header">
@@ -37,26 +44,21 @@ const Cart = () => {
           <div className="object-1">
             <Back width={30} height={30} />
           </div>
-          <div className="object-2"> Cart ({products.length})</div>
+          <div className="object-2"> Cart </div>
         </div>
       </div>
 
       <div className="main">
-        {data === undefined ? (
+        {loading ? (
           <SVGcontainer>
             <EmptyCart />
             <p className="text-3">
               No item in <b>your</b> cart yet!
             </p>
           </SVGcontainer>
-        ) : null}
-
-        {data ? (
-          <>
-            {width > breakpoint ? <CartHeader /> : null}
-            <CartData data={data} />
-          </>
-        ) : null}
+        ) : (
+          <CartData data={data.carts} refetch={refetch} />
+        )}
       </div>
 
       <Summary>
