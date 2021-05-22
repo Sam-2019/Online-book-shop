451b0c7eda999024daae6d1b1746d55448971d3b
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-22 16:37:05 +0000

    working on admin page

--- src/Pages/Admin/Admin.js
+++ src/Pages/Admin/Admin.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
 import AddBookForm from "./addProductForm";
 import ViewBooks from "./viewBooks";
 import Button from "../Components/Button";
+
+import { GET_PRODUCTS } from "../graphQL functions";
+
 import "./styles.css";
 
 const Admin = () => {
   const [form, setForm] = React.useState(false);
   const [books, setBooks] = React.useState(false);
+
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
+
+  console.log(data);
 
   return (
     <div className="admin-wrapper">
@@ -24,7 +32,7 @@ const Admin = () => {
         />
       </div>
       {form && <AddBookForm closeform={() => setForm(false)} />}
-      {books && <ViewBooks closeform={() => setBooks(false)} />}
+      {books && <ViewBooks closeform={() => setBooks(false)} data={data} />}
     </div>
   );
 };
