0c432135ceddb6ff0f832c5791886b94abc5342f
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-23 21:46:14 +0000

    removd unused code

--- src/Pages/Product/Product.js
+++ src/Pages/Product/Product.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { useHistory, useRouteMatch, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_PRODUCT } from "../graphQL functions";
 
 import Placeholder from "../Placeholders/Product";
 import ProductData from "./productData";
+import Error from "../Error/Error";
 
 import "./product.css";
 
 const Product = () => {
-  let history = useHistory();
   let { sku } = useParams();
-  let { url } = useRouteMatch();
 
   const { loading, error, data } = useQuery(GET_PRODUCT, {
-    variables: { sku },
+    variables: { sku: sku },
   });
 
+  console.log(data);
+
   if (loading) return <Placeholder />;
-  if (error) return `Error! ${error}`;
+  if (error) return <Error>{`Error! ${error}`}</Error>;
 
   return <ProductData results={data.product} />;
 };
