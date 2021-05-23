91983a0a19dda12da757d2dee8ca01ee522a4cd9
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-23 21:33:18 +0000

    added Product error page

---
+++ src/Pages/Error/ProductError.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Share from "../Components/Share";
+import Back from "../Components/Back";
+
+const ProductError = (message) => {
+  return (
+    <div className="product-wrapper">
+      <div className="header">
+        <div className="category ">
+          <div className="object-1">
+            <Back width={30} height={30} />
+          </div>
+          <div className="object-2">Error</div>
+        </div>
+
+        <div className="category ">
+          <div className="object-2">
+            <Share width={20} height={20} />
+          </div>
+        </div>
+      </div>
+
+      <div className="main">
+        <div className="wrapper">
+          <div className="product-body">
+            <div className="product-detail  ">{`Error! ${message}`}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductError;
