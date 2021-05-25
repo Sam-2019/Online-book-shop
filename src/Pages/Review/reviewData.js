ab1c14d97ddcaa9c98456b69029fbe5065e01bba
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-25 21:05:19 +0000

    repurposed reviewData

--- src/Pages/Review/reviewData.js
+++ src/Pages/Review/reviewData.js
@@ -1,50 +1,14 @@
-export const reviewData = [
-  {
-    picture: "https://randomuser.me/api/portraits/men/81.jpg",
-    name: "Sanni Huhtala",
-    time_stamp: "2017-06-13T05:23:45.965Z",
-    rating: "4",
-    comment:
-      "Urban Fantasy alien harem catgirls. Two races at war crash-landing on an alien world; one human man, two feline women, one injured, and one very smart puppy, loyal to the man who raised her. It's been done before, but rarely so well. Better, even when you know how it ends, it's good to come back to however many times you choose to revisit. Better yet, there's a sequel waiting after.",
-  },
-  {
-    picture: "https://randomuser.me/api/portraits/men/59.jpg",
-    name: "Leni Gjersvik",
-    time_stamp: "2013-05-08T12:17:21.859Z",
-    rating: "5",
-    comment:
-      "Lots of action and some amusing language in a well written book with a good plot and excellent characters.",
-  },
-  {
-    picture: "https://randomuser.me/api/portraits/men/85.jpg",
-    name: "Harper Mills",
-    time_stamp: "2003-03-18T18:02:14.021Z",
-    rating: "2",
-    comment:
-      "This book is amazing just like all other books of eliah. Story of xavier and aveena is amazing and will keep you hooked to read all book in one go. Waiting for next book!",
-  },
-  {
-    picture: "https://randomuser.me/api/portraits/men/65.jpg",
-    name: "Roméo Van Aggelen",
-    time_stamp: "2013-04-15T20:22:54.942Z",
-    rating: "1",
-    comment:
-      "Wow! I really enjoyed reading this book. Dogs, the main character, is learning about his powers and what they mean. He is a PI in LA and tries to do good. But misfortune seems to follow him like a magnet. But it is interesting to see how he solves those problems.",
-  },
-  {
-    picture: "https://randomuser.me/api/portraits/men/52.jpg",
-    name: "Elliot Singh",
-    time_stamp: "2016-06-29T07:40:24.800Z",
-    rating: "4",
-    comment:
-      "This book grabbed me on the very first page and didn’t let go! It’s one of the best I’ve read in awhile.I look forward to following this series and finding out what’s next for the Phoenix as she explores her new life!",
-  },
-  {
-    picture: "https://randomuser.me/api/portraits/men/71.jpg",
-    name: "Theo Wong",
-    time_stamp: "2018-08-12T00:08:40.449Z",
-    rating: "3",
-    comment:
-      " Curtis Pith is an amazing cook who is always cooking up something delicious for his mom and younger sister Paige. And every Wednesday night he tunes into his favorite show: The Super Chef. Curtis has a secret, the Super Chef, Lucas Taylor, is his absentee father. Things at home aren’t always easy as his single mom struggles to keep jobs and pay the rent. So, when Lucas Taylor announces the final season of the Super Chef is going to a kids only season with a grand prize of $250,000, Curtis decides to enter in the hopes that the money will help his mom out and he can finally meet his father. This is a book that is full of action and emotions. You are right there in the competition with Curtis Pith as he tries to win for his mom and navigate a relationship with his father. The story has themes of family, self-confidence, friendship, and the future. Throughout the competition the kids are learning about 5 things that make great chefs, and each is applicable to life in general. There is so much for a young reader to get from this book. ",
-  },
-];
+import React from "react";
+import ReviewItem from "./reviewItem";
+
+const ReviewData = ({ data }) => {
+  return (
+    <>
+      {data.map((item, i) => (
+        <ReviewItem {...item} key={i} />
+      ))}
+    </>
+  );
+};
+
+export default ReviewData;
