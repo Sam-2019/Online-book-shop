a51895d8c0ddb647659426405db39d9dde69c55d
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-25 21:04:38 +0000

    imported useQuery & added condition statements

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -1,11 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
 import Back from "../Components/Back";
 import ReviewItem from "./reviewItem";
-import { reviewData } from "./reviewData";
+
+import { useQuery } from "@apollo/client";
+import { GET_PRODUCT_REVIEWS } from "../graphQL functions";
 import "./review.css";
 
 const Review = () => {
+  let { sku } = useParams();
+
+  const { loading, error, data } = useQuery(GET_PRODUCT_REVIEWS, {
+    variables: { sku },
+  });
+
+  console.log(data.reviews);
+
+  let view;
+
+  if (loading) {
+    return (
+      <div className="page-wrapper">
+        <div className="header">
+          <div className="category">
+            <div className="object-1">
+              <Back width={30} height={30} />
+            </div>
+            <div className="object-2">Review</div>
+          </div>
+        </div>
+
+        <div className="main">
+          <div className=" wrapper-item">Loading...</div>
+        </div>
+      </div>
+    );
+  }
+
+  if (data.reviews.length === 0) {
+    view = <p className="text-3">No reviews yet!</p>;
+  }
+
+  if (data.reviews.length > 0) {
+    view = <ReviewItem data={data.reviews} />;
+  }
+
   return (
     <div className="page-wrapper">
       <div className="header">
@@ -13,16 +53,12 @@ const Review = () => {
           <div className="object-1">
             <Back width={30} height={30} />
           </div>
-          <div className="object-2">Review ({reviewData.length})</div>
+          <div className="object-2">Review</div>
         </div>
       </div>
 
       <div className="main">
-        <div className=" wrapper-item">
-          {reviewData.map((item, index) => (
-            <ReviewItem key={index} {...item} />
-          ))}
-        </div>
+        <div className=" wrapper-item">{view}</div>
       </div>
     </div>
   );
