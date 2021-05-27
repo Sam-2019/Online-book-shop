647883428ef78e304f1d600fae4bd95c4c12b535
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-05-27 22:28:06 +0000

    updated useLocalStorage

--- src/Pages/helper.js
+++ src/Pages/helper.js
@@ -21,7 +21,7 @@ export function useLocalStorage(key, initialValue) {
       // Get from local storage by key
       const item = window.localStorage.getItem(key);
       // Parse stored json or if none return initialValue
-      return item ? item : initialValue;
+      return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       // If error also return initialValue
       console.log(error);
@@ -38,7 +38,7 @@ export function useLocalStorage(key, initialValue) {
       // Save state
       setStoredValue(valueToStore);
       // Save to local storage
-      window.localStorage.setItem(key, valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
     } catch (error) {
       // A more advanced implementation would handle the error case
       console.log(error);
