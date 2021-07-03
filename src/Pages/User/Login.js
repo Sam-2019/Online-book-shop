d6c995ce2d2bf8fa5ee52e5a2ff66e7457a5882f
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-07-03 21:38:06 +0000

    removed console.log

--- src/Pages/User/Login.js
+++ src/Pages/User/Login.js
@@ -29,19 +29,14 @@ const Login = () => {
 
   const [loginUser, { error }] = useLazyQuery(LOGIN, {
     onCompleted: (data) => {
-      //console.log(data);
+  
       setAuth(data.login.token);
       setUniqueID(data.login.user);
 
       clearLogin();
       history.push("./");
 
-      // store the token
-      //   localStorage.setItem('token', data.signUp);
-      // update the local cache
-      //  client.writeData({ data: { isLoggedIn: true } });
-      // redirect the user to the homepage
-      //  props.history.push('/');
+
     },
   });
   let type;
