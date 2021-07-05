987c95e76bd9e1c3f5a25084116a685c38dd0eb7
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-07-05 21:17:57 +0000

    code clean-up

--- src/Pages/Data.js
+++ src/Pages/Data.js
@@ -4,53 +4,43 @@ import { useQuery } from "@apollo/client";
 import { GET_USER } from "./graphQL functions";
 
 const Data = () => {
-  const [firstName, setFirstName] = useLocalStorage("firstName", "");
-  const [lastName, setLastName] = useLocalStorage("lastName", "");
-  const [email, setEmail] = useLocalStorage("email", "");
-  const [uniqueID, setUniqueID] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
   const [verfifcationStatus, setVerificationStatus] = useState(false);
 
   const [auth, setAuth] = useLocalStorage("loginToken", "");
-
-  const token = localStorage.getItem("loginToken");
-  const id = localStorage.getItem("uniqueID");
 
   const [profileImage, setProfileImage] = useState(
     "https://i.redd.it/liptgenrd1b01.png"
   );
 
-  const { data: userInfo } = useQuery(GET_USER, {
-    variables: { id: id },
-  });
+  const { data } = useQuery(GET_USER);
+
+  console.log(data);
 
   useEffect(() => {
     let didCancel = false;
 
     // function verify() {
-    if (userInfo) {
-      setVerificationStatus(userInfo.user.verified);
-      setFirstName(userInfo.user.first_name);
-      setLastName(userInfo.user.last_name);
-      setEmail(userInfo.user.email);
-    }
-
-    if (token && id) {
-      setAuth(true);
-      setUniqueID(id);
-    }
+    // if (userInfo) {
+    //   setVerificationStatus(userInfo.user.verified);
+    //   setFirstName(userInfo.user.firstName);
+    //   setLastName(userInfo.user.lastName);
+    //   setEmail(userInfo.user.email);
+    // }
 
     // verify();
 
     return () => {
       didCancel = true;
     };
-  }, [userInfo, id, setAuth, setFirstName, setLastName, setEmail, token]);
+  }, []);
 
   function logoutUser() {
     setFirstName("");
     setLastName("");
     setEmail("");
-    setUniqueID("");
     setVerificationStatus("");
     setAuth("");
     setProfileImage("");
@@ -63,10 +53,8 @@ const Data = () => {
     firstName,
     lastName,
     email,
-    uniqueID,
     verfifcationStatus,
     setAuth,
-    setUniqueID,
 
     profileImage,
   };
