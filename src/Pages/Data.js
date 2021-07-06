61388d188d6b3352d231f6142ac0edd2acfd3f9a
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-07-06 07:42:25 +0000

    code clean-up

--- src/Pages/Data.js
+++ src/Pages/Data.js
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useLocalStorage } from "./helper";
-import { useQuery } from "@apollo/client";
-import { GET_USER } from "./graphQL functions";
 
 const Data = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
+  const [email, setUserEmail] = useState("");
   const [verfifcationStatus, setVerificationStatus] = useState(false);
 
   const [auth, setAuth] = useLocalStorage("loginToken", "");
@@ -15,32 +13,10 @@ const Data = () => {
     "https://i.redd.it/liptgenrd1b01.png"
   );
 
-  const { data } = useQuery(GET_USER);
-
-  console.log(data);
-
-  useEffect(() => {
-    let didCancel = false;
-
-    // function verify() {
-    // if (userInfo) {
-    //   setVerificationStatus(userInfo.user.verified);
-    //   setFirstName(userInfo.user.firstName);
-    //   setLastName(userInfo.user.lastName);
-    //   setEmail(userInfo.user.email);
-    // }
-
-    // verify();
-
-    return () => {
-      didCancel = true;
-    };
-  }, []);
-
   function logoutUser() {
     setFirstName("");
     setLastName("");
-    setEmail("");
+    setUserEmail("");
     setVerificationStatus("");
     setAuth("");
     setProfileImage("");
@@ -53,8 +29,13 @@ const Data = () => {
     lastName,
     email,
     verfifcationStatus,
-    setAuth,
     profileImage,
+
+    setAuth,
+    setFirstName,
+    setLastName,
+    setUserEmail,
+    setVerificationStatus,
   };
 };
 
