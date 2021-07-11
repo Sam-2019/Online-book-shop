1ccdf0b05d3d71677342e4540ce242d009fee544
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-07-11 21:43:31 +0000

    added InMemoryCache options

--- src/App.js
+++ src/App.js
@@ -35,8 +35,17 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      OrderItemType: {
+        keyFields: ["id"],
+      },
+      
+    },
+  }),
 });
+
+
 
 function App() {
   return (
